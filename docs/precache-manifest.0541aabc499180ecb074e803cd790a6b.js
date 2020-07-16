5d0872f6d730e7f6e1412b4b6f5217336d8c758a
martin jerez <martincho111986@gmail.com>
2020-07-16 19:41:44 -0300

    agregando docs en produccion

---
+++ docs/precache-manifest.0541aabc499180ecb074e803cd790a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce68d989d41c7f2589595286f158f300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c61f86f40ee5e1b10f3d",
+    "url": "/static/css/main.607297ba.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61f86f40ee5e1b10f3d",
+    "url": "/static/js/main.747910c7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
